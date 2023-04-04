f42385e7065252f4ace6515695b6f00641f11c1c
Zeizvant <oxanashvilizezva@gmail.com>
2023-04-04 14:58:35 +0400

    refactor: replace plain html url with asset function

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,7 +9,7 @@
     </head>
     <body class="w-full h-full flex justify-center gradient static">
         <div class="w-1213 mt-228 h-fit flex flex-col items-center gap-65 text-5xl text-white mb-169">
-            <img src="./images/image.png" class="w-700"/>
+            <img src="{{asset('/images/image.png')}}" class="w-700"/>
             <p>“What should I tell you your mother?!”</p>
             <a class="underline" href="#">The Son of Soldier</a>
         </div>
