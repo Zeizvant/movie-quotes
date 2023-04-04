3f5c9e87c97fa8b96ffc2aecebb151f963d5a41a
Zeizvant <oxanashvilizezva@gmail.com>
2023-04-04 19:07:57 +0400

    feat: create main layout componnet

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Movie Quotes</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="w-full h-full flex justify-center gradient static">
+        {{ $slot  }}
+
+        <div class="fixed left-[54px] inset-y-0 flex flex-col flex flex-col mt-5 lg:justify-center space-y-[15px]">
+            <div class="language-buttons">en</div>
+            <div class="language-buttons bg-white text-black bg-white ">ka</div>
+        </div>
+    </body>
+</html>
