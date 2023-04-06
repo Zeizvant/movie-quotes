d62ac47f35b537f8d61e0f0cc859830fb979c576
Zeizvant <oxanashvilizezva@gmail.com>
2023-04-06 21:43:16 +0400

    feat: add database models and their seeder

--- database/migrations/2023_04_06_163201_create_quotes_table.php
+++ database/migrations/2023_04_06_163201_create_quotes_table.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration {
+return new class extends Migration {
 	/**
 	 * Run the migrations.
 	 */
 	public function up(): void
 	{
-		Schema::create('users', function (Blueprint $table) {
+		Schema::create('quotes', function (Blueprint $table) {
 			$table->id();
-			$table->string('username')->unique();
-			$table->string('password');
-			$table->rememberToken();
+			$table->text('body');
+			$table->string('thumbnail')->nullable();
+			$table->foreignId('movie_id')->constrained()->cascadeOnDelete();
 			$table->timestamps();
 		});
 	}
@@ -24,6 +24,6 @@ return new class() extends Migration {
 	 */
 	public function down(): void
 	{
-		Schema::dropIfExists('users');
+		Schema::dropIfExists('quotes');
 	}
 };
