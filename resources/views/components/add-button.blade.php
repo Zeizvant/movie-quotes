fa8c7c9e896641175915594bae09242cefb0016b
Zeizvant <oxanashvilizezva@gmail.com>
2023-04-10 21:55:04 +0400

    style: change add button pointing area

--- resources/views/components/add-button.blade.php
+++ resources/views/components/add-button.blade.php
@@ -1,4 +1,6 @@
 @props(['text', 'link'])
-<div class="cursor-pointer rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-    <a href="{{ route("{$link}.create") }}">{{ $text }}</a>
-</div>
+<a href="{{ route("{$link}.create") }}">
+    <div class="cursor-pointer rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        {{ $text }}
+    </div>
+</a>
