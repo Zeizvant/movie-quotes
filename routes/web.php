f10b3ffe53edd3150cb21496d75ad89f1f0458bf
Zeizvant <oxanashvilizezva@gmail.com>
2023-04-13 12:01:14 +0400

    refactor: refactor admin routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LanguageController;
 
 /*
@@ -27,13 +26,9 @@ Route::controller(AuthController::class)->group(function () {
 	Route::post('/logout', 'logout')->middleware('auth')->name('auth.logout');
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => ['auth'], 'controller' => AdminController::class], function () {
-	Route::get('quotes', 'showQuotes')->name('admin.quote.show');
-	Route::get('movies', 'showMovies')->name('admin.movie.show');
-});
-
 Route::controller(MovieController::class)->group(function () {
 	Route::get('/movie/{movie}', 'show')->name('movie.show');
+	Route::get('/admin/movies', 'showMovies')->middleware('auth')->name('admin.movie.show');
 });
 
 Route::group(['prefix' => 'movies', 'middleware' => ['auth'], 'controller' => MovieController::class], function () {
@@ -46,11 +41,7 @@ Route::group(['prefix' => 'movies', 'middleware' => ['auth'], 'controller' => Mo
 
 Route::controller(QuoteController::class)->group(function () {
 	Route::get('/', 'show')->name('quote.show');
-	//	Route::delete('/quotes/{quote}', 'delete')->middleware('auth')->name('quote.delete');
-	//	Route::get('/quotes/create', 'create')->middleware('auth')->name('quote.create');
-	//	Route::post('/quotes', 'store')->middleware('auth')->name('quote.store');
-	//	Route::get('/quotes/{quote}/edit', 'edit')->middleware('auth')->name('quote.edit');
-	//	Route::put('/quotes/{quote}', 'update')->middleware('auth')->name('quote.update');
+	Route::get('/admin/quotes', 'showQuotes')->middleware('auth')->name('admin.quote.show');
 });
 
 Route::group(['prefix' => 'quotes', 'middleware' => ['auth'], 'controller' => QuoteController::class], function () {
