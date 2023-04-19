00427c971ecac290406f22d1b00068191bd78814
Zeizvant <oxanashvilizezva@gmail.com>
2023-04-19 20:05:14 +0400

    style: add space between inputs in login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,7 +13,7 @@
             <form class="mt-8 space-y-6" action="{{ route('auth.login') }}" method="POST">
                 @csrf
                 <input type="hidden" name="remember" value="true">
-                <div class="-space-y-px rounded-md shadow-sm">
+                <div class="-space-y-px rounded-md shadow-sm flex flex-col gap-2">
                     <div>
                         <input id="username" name="username" type="text" autocomplete="username" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="{{ __('forms.username') }}">
                         @error('username')
